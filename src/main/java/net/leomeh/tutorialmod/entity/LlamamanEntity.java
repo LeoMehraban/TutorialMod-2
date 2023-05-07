58cdcfe81642c07a1dcdffb9fde344c7fb5d6e15
LeoMehraban <62709564+LeoMehraban@users.noreply.github.com>
2023-05-07 11:00:30 -0400

    dfh

--- src/main/java/net/leomeh/tutorialmod/entity/LlamamanEntity.java
+++ src/main/java/net/leomeh/tutorialmod/entity/LlamamanEntity.java
@@ -23,6 +23,7 @@ import net.minecraft.world.entity.projectile.LlamaSpit;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -33,25 +34,30 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class LlamamanEntity extends Monster implements RangedAttackMob, IAnimatable {
+import java.util.UUID;
+
+public class LlamamanEntity extends Monster implements RangedAttackMob, IAnimatable /*,NeutralMob */ {
     private AnimationFactory factory = GeckoLibUtil.createFactory(this);
+
     public LlamamanEntity(EntityType<LlamamanEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
+
     public static AttributeSupplier setAttributes() {
         return Monster.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0D)
                 .add(Attributes.ATTACK_DAMAGE, 3.0f)
-                .add(Attributes.ATTACK_SPEED, 0.1f)
+                .add(Attributes.ATTACK_SPEED, 0.4f)
                 .add(Attributes.MOVEMENT_SPEED, 0.01f).build();
     }
 
 
     private boolean didSpit = false;
+
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 0.5D, true));
+        this.goalSelector.addGoal(2, new MoveTowardsTargetGoal(this, 1D, 5f));
         this.goalSelector.addGoal(3, new RangedAttackGoal(this, 1.25D, 40, 10.0F));
         this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
@@ -63,23 +69,20 @@ public class LlamamanEntity extends Monster implements RangedAttackMob, IAnimata
     }
 
 
-
     private void spit(LivingEntity pTarget) {
         LlamamanSpit llamaspit = new LlamamanSpit(this.level, this);
         double d0 = pTarget.getX() - this.getX();
         double d1 = pTarget.getY(0.3333333333333333D) - llamaspit.getY();
         double d2 = pTarget.getZ() - this.getZ();
-        double d3 = Math.sqrt(d0 * d0 + d2 * d2) * (double)0.2F;
+        double d3 = Math.sqrt(d0 * d0 + d2 * d2) * (double) 0.2F;
         llamaspit.shoot(d0, d1 + d3, d2, 1.5F, 10.0F);
         if (!this.isSilent()) {
-            this.level.playSound((Player)null, this.getX(), this.getY(), this.getZ(), SoundEvents.LLAMA_SPIT, this.getSoundSource(), 1.0F, 1.0F + (this.random.nextFloat() - this.random.nextFloat()) * 0.2F);
+            this.level.playSound((Player) null, this.getX(), this.getY(), this.getZ(), SoundEvents.LLAMA_SPIT, this.getSoundSource(), 1.0F, 1.0F + (this.random.nextFloat() - this.random.nextFloat()) * 0.2F);
         }
 
         this.level.addFreshEntity(llamaspit);
         this.didSpit = true;
     }
-
-
 
 
     @Override
@@ -91,7 +94,6 @@ public class LlamamanEntity extends Monster implements RangedAttackMob, IAnimata
     protected void defineSynchedData() {
         super.defineSynchedData();
     }
-
 
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
@@ -113,6 +115,6 @@ public class LlamamanEntity extends Monster implements RangedAttackMob, IAnimata
     public AnimationFactory getFactory() {
         return factory;
     }
-
-
 }
+
+
